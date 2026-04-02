b43ad10eb92b3e36a2ee3b3d8c2fc59a546a70e2
West Point (NPH) Account <142028542+nph-westpoint@users.noreply.github.com>
2026-04-02 15:50:10 -0500

    Update cgm_object.py

--- read_cgm_data/src/cgm_object.py
+++ read_cgm_data/src/cgm_object.py
@@ -1214,18 +1214,12 @@ class CGM(object):
                  }
         ax4= self.ax_stats(ax4,**kwargs
                     )  
-        stats = np.zeros(3)
-        st.write(stats)
+        stats = np.zeros(2)
         stats1 = self.calc_stat('auc',group_by='day').values.reshape(-1)
-        st.write(stats1[0], stats1[1])
         stats[0] = stats1[1]
         stats[1] = stats1[0]
-        st.write(stats)
         new_stat = self.calc_stat('auc').values.reshape(-1)
-        st.write(new_stat)
-        #stats[2] = self.calc_stat('auc').values.reshape(-1)
         stats = np.hstack([stats,new_stat])
-        st.write(stats)
 
         ax5 = plt.subplot2grid(fs,(4,0),rowspan=4,colspan=3)
         scols = ['auc_wake','auc_sleep','auc_all']
