32621fdec4d8bb8b2820eada311552caac7c0b33
West Point (NPH) Account <142028542+nph-westpoint@users.noreply.github.com>
2026-07-13 06:22:10 -0500

    Update app_launch.py

--- app_launch.py
+++ app_launch.py
@@ -73,7 +73,7 @@ if select == options[0]:
     st.subheader(":red[Local Execution (Maximum Privacy)]")
     body = "Users who prefer not to upload data to the Streamlit-hosted application may " 
     body+= "instead run GVC-Calc locally on their own computer using the Docker version "
-    body+= "available from the GitHub repository: [GVC-Calc Github](https://github.com/nph-westpoint/gvc-calc)."
+    body+= "available from the GitHub repository: [GVC-Calc Github](https://github.com/nph-westpoint/gvc-calc). "
     body+= "When run locally, all data processing and calculations occur entirely on the "
     body+= "user's computer. In this setting, CGM data are not transmitted to external servers."
     st.markdown(body)
